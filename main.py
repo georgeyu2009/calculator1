8ac1674474db39dbd24a6241cb1a275f8b52c84c
your_username <by29@uw.edu>
2023-07-07 11:26:59 -0700

    completed layout

--- main.py
+++ main.py
@@ -1,3 +1,25 @@
-import numpy
-import kivy
+from kivy.app import App
+from kivy.uix.widget import Widget
+from kivy.lang import Builder
+from kivy.core.window import Window
+
+
+Window.size = (500, 700)
+Builder.load_file("style.kv")
+
+class MyLayout(Widget):
+
+    def press(self):
+        self.ids.calc_input.text = "0"
+
+
+
+class MyApp(App):
+    def build(self):
+        return MyLayout()
+
+
+if __name__ == '__main__':
+    MyApp().run()
+
 
